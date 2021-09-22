99cbd07f8bfad86176712b1917c2c8704d4aa8f5
AzureAD\GabrielNegreanu <gabriel.negreanu@principal33.com>
2021-09-22 23:48:12 +0300

    My backend for CMS project

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EmployeeApi.Models;
+using Microsoft.AspNetCore.Cors;
+
+namespace EmployeeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeContext _context;
+
+        public EmployeeController(EmployeeContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Employee
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            return await _context.Employees
+                .OrderBy(employee => employee.Name)
+                .ToListAsync();
+        }
+
+        // GET: api/Employee/search/{searchTerm}
+        [HttpGet("search/{search}")]
+        public async Task<ActionResult<IEnumerable<Employee>>> SearchEmployees(string search)
+        {
+            return await _context.Employees
+                .Where(employee => employee.Name.Contains(search) || employee.Email.Contains(search))
+                .OrderBy(employee => employee.Name)
+                .ToListAsync();
+        }
+
+        // GET: api/Employee/filter/{gender}
+        [HttpGet("filter/{gender}")]
+        public async Task<ActionResult<IEnumerable<Employee>>> FilterEmployees(string gender)
+        {
+            return await _context.Employees
+                .Where(employee => employee.Gender.Equals(gender))
+                .OrderBy(employee => employee.Name)
+                .ToListAsync();
+        }
+
+        // GET: api/Employee/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(long id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return employee;
+        }
+
+        // PUT: api/Employees/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployee(long id, Employee employee)
+        {
+            if (id != employee.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employee).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Employees
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Employee>> Postemployee(Employee employee)
+        {
+            
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetEmployee), new { id = employee.Id }, employee);
+        }
+
+        // DELETE: api/Employees/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(long id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmployeeExists(long id)
+        {
+            return _context.Employees.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeApi.Models
+{
+    public class Employee
+    {
+        public long Id { get; set; }
+        public string ProfilePicture { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string Birthdate { get; set; }
+    }
+}
